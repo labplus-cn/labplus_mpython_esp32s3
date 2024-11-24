558dcc50bfe7ff58b0616a5b65c622db7f14c9de
diskman88 <diskman88@163.com>
2024-11-24 14:03:36 +0800

    add merge firmware script: 	modified:   port/CMakeLists.txt 	modified:   port/boards/labplus_Ledong_pro/partitions-8MiB.csv 	modified:   port/makeimg.py 	modified:   "port/\345\215\207\347\272\247\345\210\260micropython_v1.23.0.md"

--- port/makeimg.py
+++ port/makeimg.py
@@ -1,5 +1,9 @@
 # Combine bootloader, partition table and application into a final binary.
+from __future__ import print_function
 
+import argparse
+import datetime
+import subprocess
 import os, sys
 
 sys.path.append(os.getenv("IDF_PATH") + "/components/partition_table")
@@ -9,6 +13,47 @@ import gen_esp32part
 OFFSET_BOOTLOADER_DEFAULT = 0x1000
 OFFSET_PARTITIONS_DEFAULT = 0x8000
 
+def get_version_info_from_git(repo_path):
+    # Python 2.6 doesn't have check_output, so check for that
+    try:
+        subprocess.check_output
+    except AttributeError:
+        return None
+
+    # Note: git describe doesn't work if no tag is available
+    try:
+        git_tag = subprocess.check_output(
+            ["git", "describe", "--tags", "--dirty", "--always", "--match", "v[1-9].*"],
+            cwd=repo_path,
+            stderr=subprocess.STDOUT,
+            universal_newlines=True,
+        ).strip()
+        # Turn git-describe's output into semver compatible (dot-separated
+        # identifiers inside the prerelease field).
+        git_tag = git_tag.split("-", 1)
+        if len(git_tag) == 1:
+            return git_tag[0]
+        else:
+            return git_tag[0] + "-" + git_tag[1].replace("-", ".")
+    except (subprocess.CalledProcessError, OSError):
+        return None
+
+def get_hash_from_git(repo_path):
+    # Python 2.6 doesn't have check_output, so check for that.
+    try:
+        subprocess.check_output
+    except AttributeError:
+        return None
+
+    try:
+        return subprocess.check_output(
+            ["git", "rev-parse", "--short", "HEAD"],
+            cwd=repo_path,
+            stderr=subprocess.STDOUT,
+            universal_newlines=True,
+        ).strip()
+    except (subprocess.CalledProcessError, OSError):
+        return None
 
 def load_sdkconfig_value(filename, value, default):
     value = "CONFIG_" + value + "="
@@ -39,12 +84,27 @@ def load_partition_table(filename):
 
 
 # Extract command-line arguments.
-arg_sdkconfig = sys.argv[1]
-arg_bootloader_bin = sys.argv[2]
-arg_partitions_bin = sys.argv[3]
-arg_application_bin = sys.argv[4]
-arg_output_bin = sys.argv[5]
-arg_output_uf2 = sys.argv[6]
+# arg_sdkconfig = sys.argv[1]
+# arg_bootloader_bin = sys.argv[2]
+# arg_partitions_bin = sys.argv[3]
+# arg_application_bin = sys.argv[4]
+# arg_output_bin = sys.argv[5]
+# arg_output_uf2 = sys.argv[6]
+arg_board_name = sys.argv[1]
+git_tag = get_version_info_from_git("../")
+git_hash = get_hash_from_git("../")
+build_date = datetime.date.today().strftime("%Y-%m-%d")
+bin_name = "{}-{}-{}_{}.bin".format(arg_board_name, git_tag, git_hash, build_date)
+uf2_name = "{}-{}-{}_{}.uf2".format(arg_board_name, git_tag, git_hash, build_date)
+print(bin_name)
+
+arg_sdkconfig = "sdkconfig"
+arg_bootloader_bin = "bootloader/bootloader.bin"
+arg_partitions_bin = "partition_table/partition-table.bin"
+arg_application_bin = "micropython.bin"
+arg_output_bin = bin_name
+arg_output_uf2= uf2_name
+arg_font_bin = "../Noto_Sans_CJK_SC_Light16.bin"
 
 # Load required sdkconfig values.
 idf_target = load_sdkconfig_str_value(arg_sdkconfig, "IDF_TARGET", "").upper()
@@ -62,11 +122,15 @@ max_size_bootloader = offset_partitions - offset_bootloader
 max_size_partitions = 0
 offset_application = 0
 max_size_application = 0
+offset_font = 0
 
 # Inspect the partition table to find offsets and maximum sizes.
 for part in partition_table:
     if part.name == "nvs":
         max_size_partitions = part.offset - offset_partitions
+    elif part.name == "font":
+        offset_font =  part.offset
+        max_size_font = part.size
     elif part.type == gen_esp32part.APP_TYPE and offset_application == 0:
         offset_application = part.offset
         max_size_application = part.size
@@ -76,6 +140,7 @@ files_in = [
     ("bootloader", offset_bootloader, max_size_bootloader, arg_bootloader_bin),
     ("partitions", offset_partitions, max_size_partitions, arg_partitions_bin),
     ("application", offset_application, max_size_application, arg_application_bin),
+    ("font", offset_font, max_size_font, arg_font_bin),
 ]
 file_out = arg_output_bin
 
@@ -104,7 +169,7 @@ with open(file_out, "wb") as fout:
 
 # Generate .uf2 file if the SoC has native USB.
 if idf_target in ("ESP32S2", "ESP32S3"):
-    sys.path.append(os.path.join(os.path.dirname(__file__), "../../tools"))
+    sys.path.append(os.path.join(os.path.dirname(__file__), "../../micropython/tools"))
     import uf2conv
 
     families = uf2conv.load_families()
--- port/升级到micropython_v1.23.0.md
+++ port/升级到micropython_v1.23.0.md
@@ -189,7 +189,12 @@ linux系统按乐鑫要求安装一些库，安装gcc cmake
     idf.py -D MICROPY_BOARD=labplus_Ledong_pro build
     idf.py -D MICROPY_BOARD=mpython build
     ```
-
+    合并固件
+    ```
+    idf.py -D MICROPY_BOARD=labplus_Ledong_pro merge_bin
+    idf.py -D MICROPY_BOARD=labplus_Ledong_pro build merge_bin
+    ```
+    
 5. 固件烧录
 
     ```bash
