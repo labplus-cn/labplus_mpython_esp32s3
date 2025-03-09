317b9fa7eee043d7a569195ed3c06174517a6832
diskman88 <diskman88@163.com>
2025-03-09 20:16:40 +0800

    修改LCD、camera驱动: 	modified:   port/usercmodule/esp-who/esp-who 	modified:   port/usercmodule/esp-who/mod/test.cpp

--- port/usercmodule/esp-who/mod/test.cpp
+++ port/usercmodule/esp-who/mod/test.cpp
@@ -18,10 +18,10 @@ mp_obj_t esp_who_test(void)
     xQueueAIFrame = xQueueCreate(2, sizeof(camera_fb_t *));
     xQueueLCDFrame = xQueueCreate(2, sizeof(camera_fb_t *));
 
-    register_camera(PIXFORMAT_RGB565, FRAMESIZE_240X240, 2, xQueueAIFrame);
+    register_camera(PIXFORMAT_RGB565, FRAMESIZE_QVGA, 2, xQueueAIFrame);
     register_human_face_detection(xQueueAIFrame, NULL, NULL, xQueueLCDFrame, false);
     // register_cat_face_detection(xQueueAIFrame, NULL, NULL, xQueueLCDFrame, false);
-    // register_lcd(xQueueLCDFrame, NULL, true);
+    register_lcd(xQueueLCDFrame, NULL, true);
     return mp_const_none; 
 }
 
