7b79db0b89dd3ec625f5414267a9b76d95f6ee9e
diskman88 <diskman88@163.com>
2025-03-11 22:46:42 +0800

    把lv_binding_micropython 做为子模块加入项目: 分支：update/micropython_v1.24.1_usermod。 	modified:   .gitmodules 	new file:   port/usercmodule/lv_binding_micropython 	modified:   port/usercmodule/micropython.cmake

--- port/usercmodule/micropython.cmake
+++ port/usercmodule/micropython.cmake
@@ -1,6 +1,6 @@
 # Add the lv_binding_micropython.
 if(MPYTHON_PRO_BOARD OR LABPLUS_LEDONG_V2_BOARD)
-include(${CMAKE_CURRENT_LIST_DIR}/lv_binding_micropython/bindings.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/lv_binding_micropython/micropython.cmake)
 
 # add lcd driver module
 include(${CMAKE_CURRENT_LIST_DIR}/lcd/micropython.cmake)
