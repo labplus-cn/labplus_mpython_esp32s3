16180457d18872fc123027d4a256f66bfd77528a
diskman88 <diskman88@163.com>
2025-03-20 23:00:13 +0800

    AIcamera模块编写，未完: 	modified:   port/usercmodule/esp-who/esp-who 	modified:   port/usercmodule/esp-who/micropython.cmake 	modified:   port/usercmodule/esp-who/mod/modAIcamera.c

--- port/usercmodule/esp-who/micropython.cmake
+++ port/usercmodule/esp-who/micropython.cmake
@@ -3,6 +3,17 @@
 # list(APPEND EXTRA_COMPONENT_DIRS ${CMAKE_CURRENT_LIST_DIR}/esp-who/components/modules)
 # list(APPEND EXTRA_COMPONENT_DIRS ${CMAKE_CURRENT_LIST_DIR}/esp-who/components/fb_gfx)
 # list(APPEND EXTRA_COMPONENT_DIRS ${CMAKE_CURRENT_LIST_DIR}/esp-who/components/esp32-camera)
+set(ESP_DL_INCLUDEDIRS ${CMAKE_CURRENT_LIST_DIR}/esp-who/components/esp-dl/include)
+set(COMPONENT_ADD_INCLUDEDIRS   ${ESP_DL_INCLUDEDIRS}/
+                                ${ESP_DL_INCLUDEDIRS}/tool
+                                ${ESP_DL_INCLUDEDIRS}/typedef
+                                ${ESP_DL_INCLUDEDIRS}/image
+                                ${ESP_DL_INCLUDEDIRS}/math
+                                ${ESP_DL_INCLUDEDIRS}/nn
+				${ESP_DL_INCLUDEDIRS}/tvm
+                                ${ESP_DL_INCLUDEDIRS}/layer
+                                ${ESP_DL_INCLUDEDIRS}/detect
+                                ${ESP_DL_INCLUDEDIRS}/model_zoo)
 
 add_library(usermod_lcd INTERFACE)
 
@@ -15,7 +26,7 @@ set(ESP_COMPONENTS_LCD_INCLUDEDIRS
 )
 
 set(MODULES_INCLUDEDIRS 
-        ${CMAKE_CURRENT_LIST_DIR}/esp-who/components/modules/ai
+        # ${CMAKE_CURRENT_LIST_DIR}/esp-who/components/modules/ai
         ${CMAKE_CURRENT_LIST_DIR}/esp-who/components/modules/camera
         ${CMAKE_CURRENT_LIST_DIR}/esp-who/components/modules/lcd
         ${CMAKE_CURRENT_LIST_DIR}/esp-who/components/modules/web
@@ -47,5 +58,23 @@ if(LABPLUS_LEDONG_V2_BOARD)
         )
 
         target_link_libraries(usermod INTERFACE usermod_sensor)
+
+        add_library(usermod_AIcamera INTERFACE)
+
+        target_sources(usermod_AIcamera INTERFACE
+                ${CMAKE_CURRENT_LIST_DIR}/mod/ai/human_face_detection.cpp
+                ${CMAKE_CURRENT_LIST_DIR}/mod/modAIcamera.c
+        )
+
+        target_include_directories(usermod_AIcamera INTERFACE
+                ${COMPONENT_ADD_INCLUDEDIRS}
+                ${MODULES_INCLUDEDIRS}
+                ${ESP32_CAMERA_INCLUDEDIRS}
+                ${CMAKE_CURRENT_LIST_DIR}/mod/ai
+        )
+
+        target_link_libraries(usermod INTERFACE usermod_AIcamera)
 endif()
+
+add_compile_options(-fdiagnostics-color=always)
 
--- port/usercmodule/esp-who/mod/modAIcamera.c
+++ port/usercmodule/esp-who/mod/modAIcamera.c
@@ -1,52 +1,39 @@
+// #include "human_face_detection.hpp"
+
 #include "py/runtime.h"
 #include "who_camera.h"
 #include "esp_camera.h"
 #include "esp_log.h"
-#include "driver/gpio.h"
-#include "lcd.h"
-
-camera_fb_t *frame = NULL;
-
-static mp_obj_t reset(void)
+#include "who_lcd.h"
+extern bool human_face_detection(camera_fb_t *frame);
+static void detect_task_handler(void *arg)
 {
-    camera_init(PIXFORMAT_RGB565, FRAMESIZE_320X172, 2);
+    camera_fb_t *frame = NULL;
+
+    while(1){
+        frame = esp_camera_fb_get();
+        if(frame){
+            human_face_detection(frame);
+
+            lcd_draw_image(0, 0, frame->width, frame->height, (void *)frame->buf);
+            esp_camera_fb_return(frame);
+        }
+    }
+}
+
+static mp_obj_t AI_detect_init(void)
+{
+    xTaskCreatePinnedToCore(detect_task_handler, "task", 4 * 1024, NULL, 5, NULL, 0);
 
     return mp_const_none; 
 }
-static MP_DEFINE_CONST_FUN_OBJ_0(sensor_reset_obj, reset);
-
-static mp_obj_t free_fb(void)
-{
-    esp_camera_fb_return(frame);
-
-    return mp_const_none;
-}
-static MP_DEFINE_CONST_FUN_OBJ_0(sensor_free_fb_obj, free_fb);
-
-mp_obj_t sensor_init(void)
-{
-    if(!frame){
-        frame = calloc(1, sizeof(camera_fb_t));
-    }
-    return mp_obj_new_int_from_uint(0);
-}
-static MP_DEFINE_CONST_FUN_OBJ_0(sensor_init_obj, sensor_init);
-
-mp_obj_t sensor_deinit(void)
-{
-    if(frame){
-        free(frame);
-    }
-    return mp_obj_new_int_from_uint(0);
-}
-static MP_DEFINE_CONST_FUN_OBJ_0(sensor_deinit_obj, sensor_deinit);
+static MP_DEFINE_CONST_FUN_OBJ_0(AI_detect_init_obj, AI_detect_init);
 
 static const mp_rom_map_elem_t AIcamera_module_globals_table[] = {
     { MP_ROM_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_AIcamera) },
-    { MP_ROM_QSTR(MP_QSTR___init__), MP_ROM_PTR(&sensor_init_obj) },
-    { MP_ROM_QSTR(MP_QSTR___del__), MP_ROM_PTR(&sensor_deinit_obj) },
-    { MP_ROM_QSTR(MP_QSTR_get_frame_width), MP_ROM_PTR(&get_frame_width_obj) },
-    { MP_ROM_QSTR(MP_QSTR_deinit), MP_ROM_PTR(&sensor_deinit_obj) },
+    { MP_ROM_QSTR(MP_QSTR___init__), MP_ROM_PTR(&AI_detect_init_obj) },
+    // { MP_ROM_QSTR(MP_QSTR___del__), MP_ROM_PTR(&sensor_deinit_obj) },
+    // { MP_ROM_QSTR(MP_QSTR_face_detact), MP_ROM_PTR(&face_detect_obj) },
 };
 static MP_DEFINE_CONST_DICT(AIcamera_module_globals, AIcamera_module_globals_table);
 
@@ -57,4 +44,6 @@ const mp_obj_module_t AIcamera_cmodule = {
 };
 
 // Register the module to make it available in Python.
-MP_REGISTER_MODULE(MP_QSTR_sensor, AIcamera_cmodule);
+MP_REGISTER_MODULE(MP_QSTR_AIcamera, AIcamera_cmodule);
+
+
